f231edb0ccc182350e5e68b323fe9dd7017194c2
wSeungMi <seungmi.dev@gmail.com>
2024-04-03 15:20:04 +0900

    Feat: 자식 컴포넌트에 데이터 전달 연습

--- JavaScript/01_api-Moviesite/src/App.js
+++ JavaScript/01_api-Moviesite/src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "./core/heropy";
+import FruitItem from "./components/FruitItem";
 
 // 상속
 export default class App extends Component {
@@ -25,12 +26,21 @@ export default class App extends Component {
   render() {
     this.el.innerHTML = `
       <h1>Fruits</h1>
-      <ul>
-        ${this.state.fruites
-          .filter((fruit) => fruit.price < 3000)
-          .map((fruit) => `<li>${fruit.name}</li>`)
-          .join("")}
-      </ul>
+      <ul></ul>
     `;
+    const ulEl = this.el.querySelector("ul");
+    ulEl.append(
+      ...this.state.fruites
+        // .filter((fruit) => fruit.price < 3000)
+        .map(
+          (fruit) =>
+            new FruitItem({
+              props: {
+                name: fruit.name,
+                price: fruit.price,
+              },
+            }).el
+        )
+    );
   }
 }
--- JavaScript/01_api-Moviesite/src/core/heropy.js
+++ JavaScript/01_api-Moviesite/src/core/heropy.js
@@ -2,9 +2,10 @@
 export class Component {
   // 원하는 태그를 동적으로 처리하기 위해 매개변수로 태그명 받아옴
   constructor(payload = {}) {
-    const { tagName = "div", state = {} } = payload; // tagName의 기본값으로 div 할당
+    const { tagName = "div", state = {}, props = {} } = payload; // tagName의 기본값으로 div 할당
     this.el = document.createElement(tagName);
     this.state = state;
+    this.props = props;
     this.render();
   }
   render() {
