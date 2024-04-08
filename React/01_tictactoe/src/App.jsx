1408f5ee8eb3982e31b40050b785746e9b9510c4
wSeungMi <seungmi.dev@gmail.com>
2024-04-08 18:17:06 +0900

    Design: tictactoe 기본 스타일링

--- React/01_tictactoe/src/App.jsx
+++ React/01_tictactoe/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
     <>
       <div className="game">
-        <div className="gamd-board">ddd</div>
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <div>{/* TODO */}</div>
+        <div className="gamd-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          game-info
+          <div>{/* status */}</div>
+          <div>{/* TODO */}</div>
+        </div>
       </div>
     </>
   );
---
+++ React/01_tictactoe/src/components/Square.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Square.css";
+
+// 클래스형 컴포넌트
+export class Square extends React.Component {
+  render() {
+    return <button className="square"></button>;
+  }
+}
