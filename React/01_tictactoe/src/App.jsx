120844c68e751a277cd00d1d8d2815a0f4e370b6
wSeungMi <seungmi.dev@gmail.com>
2024-04-08 17:31:27 +0900

    Chore: React TicTacToe 기본 세팅

---
+++ React/01_tictactoe/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="game">
+        <div className="gamd-board">ddd</div>
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <div>{/* TODO */}</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
