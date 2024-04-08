054e4fc5a2eda2e6a77f7e896d947c71d1d9483b
wSeungMi <seungmi.dev@gmail.com>
2024-04-08 15:33:50 +0900

    Feat: 데이터 fetch 로딩 애니메이션 추가

--- JavaScript/01-1_movie-app/src/store/movie.js
+++ JavaScript/01-1_movie-app/src/store/movie.js
@@ -5,12 +5,14 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
+  loading: false,
 });
 
 export default store;
 
 // data fetch
 export const searchMovies = async (page) => {
+  store.state.loading = true;
   store.state.page = page;
   // 만약 첫 페이지를 받아온다면 페이지 & 화면 초기화
   if (page === 1) {
@@ -23,4 +25,5 @@ export const searchMovies = async (page) => {
   // 기존에 불러온 페이지 번호가 있을 수 있기에 불변성을 지켜주자!
   store.state.movies = [...store.state.movies, ...Search];
   store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+  store.state.loading = false;
 };
