ba6502e7377b28377be2a6f972725b333ca04718
wSeungMi <seungmi.dev@gmail.com>
2024-04-08 13:52:48 +0900

    Feat: MovieItem 컴포넌트 생성 및 스타일링, 기능 구현

--- JavaScript/01-1_movie-app/src/components/MovieList.js
+++ JavaScript/01-1_movie-app/src/components/MovieList.js
@@ -1,5 +1,6 @@
 import { Component } from "../../core/heropy";
 import movieStore from "../store/movie";
+import MovieItem from "./MovieItem";
 
 export default class MovieList extends Component {
   constructor() {
@@ -15,10 +16,14 @@ export default class MovieList extends Component {
     `;
 
     const moviesEl = this.el.querySelector(".movies");
+    // 전달해줘야하는 데이터 형식은 객체
     moviesEl.append(
-      movieStore.state.movies.map((movie) => {
-        return movie.Title;
-      })
+      ...movieStore.state.movies.map(
+        (movie) =>
+          new MovieItem({
+            movie,
+          }).el
+      )
     );
   }
 }
