d2745686314d71478c3c7e28d09a33f38fe8e806
wSeungMi <seungmi.dev@gmail.com>
2024-04-09 13:02:17 +0900

    Feat: 상세 페이지 라우팅 및 페이지 작업

--- JavaScript/01-1_movie-app/src/routes/index.js
+++ JavaScript/01-1_movie-app/src/routes/index.js
@@ -1,9 +1,14 @@
 import { createRouter } from "../../core/heropy";
 import Home from "./Home";
+import Movie from "./Movie";
 
 export default createRouter([
   {
     path: "#/",
     component: Home,
+  },
+  {
+    path: "#/movie",
+    component: Movie,
   },
 ]);
