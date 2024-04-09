d80d9429b355769f69e1320aea7d66341f9b9fe5
wSeungMi <seungmi.dev@gmail.com>
2024-04-09 17:04:46 +0900

    Feat: 404 Page Not Found 추가

--- JavaScript/01-1_movie-app/src/routes/index.js
+++ JavaScript/01-1_movie-app/src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter } from "../../core/heropy";
 import Home from "./Home";
 import Movie from "./Movie";
 import About from "./About";
+import NotFound from "./NotFound";
 
 export default createRouter([
   {
@@ -15,5 +16,9 @@ export default createRouter([
   {
     path: "#/about",
     component: About,
+  },
+  {
+    path: ".*",
+    component: NotFound,
   },
 ]);
